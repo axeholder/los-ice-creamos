498976ca1070c5e3ae93bf1d7b94d9bf0155d731
Vitaliy Myts <74954880+Zombik007@users.noreply.github.com>
2021-02-24 02:58:45 +0200

    Добавляет анимацию кнопок и скрывает кнопки локаций на карте

--- src/js/contacts-map.js
+++ src/js/contacts-map.js
@@ -6,17 +6,34 @@
     openFranchiseBtn: document.querySelector('[data-modal-franchise-open]'),
     closeFranchiseBtn: document.querySelector('[data-modal-franchise-close]'),
     modalFranchise: document.querySelector('[data-modal-franchise]'),
+    openLocationButton: document.querySelector('[open-location-button]'),
+    locationList: document.querySelector('[open-location-list]'),
+    locationButtonNY: document.querySelector('[location-button-ny]'),
+    locationButtonCH: document.querySelector('[location-button-ch]'),
+    locationButtonLA: document.querySelector('[location-button-la]'),
   };
 
   refs.openMapBtn.addEventListener('click', toggleModalMap);
   refs.closeMapBtn.addEventListener('click', toggleModalMap);
   refs.openFranchiseBtn.addEventListener('click', toggleModalFranchise);
   refs.closeFranchiseBtn.addEventListener('click', toggleModalFranchise);
+  refs.openLocationButton.addEventListener('click', toggleOpenLocationList);
+  refs.locationButtonNY.addEventListener('click', toggleOpenLocationList);
+  refs.locationButtonCH.addEventListener('click', toggleOpenLocationList);
+  refs.locationButtonLA.addEventListener('click', toggleOpenLocationList);
 
   function toggleModalMap() {
-    refs.modalMap.classList.toggle('contacts-backdrop-hidden');
+    refs.modalMap.classList.toggle('location-backdrop-hidden');
   }
+
   function toggleModalFranchise() {
     refs.modalFranchise.classList.toggle('franchise-backdrop-hidden');
+  }
+
+  function toggleOpenLocationList() {
+    refs.locationList.classList.toggle('location-button-list-open');
+    refs.locationButtonNY.classList.toggle('button-animated-in');
+    refs.locationButtonCH.classList.toggle('button-animated-in');
+    refs.locationButtonLA.classList.toggle('button-animated-in');
   }
 })();
