1aa89cc8a32285be6379683df9ec28b59c163e9e
Maria Belik <mashabelik@gmail.com>
2021-02-26 12:16:03 +0200

    перемещает js и pics по своим папкам about

--- src/js/about/about-backdrop-click.js
+++ src/js/about/about-backdrop-click.js
@@ -20,3 +20,14 @@
     }
 
 })();
+
+(() => {
+
+  document.querySelector('.js-speaker-form').addEventListener('submit', e => {
+    e.preventDefault();
+
+    new FormData(e.currentTarget).forEach((value, name) =>
+      console.log(`${name}: ${value}`),
+    );
+  });
+})();
